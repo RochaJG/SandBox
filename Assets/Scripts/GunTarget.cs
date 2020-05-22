cd9753b0c41a372be5e70e4b49248474242fd6cd
Jordan Rocha <70rd23@gmail.com>
2020-05-22 01:16:25 -0300

    Added basic weapon system

---
+++ Assets/Scripts/GunTarget.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GunTarget : MonoBehaviour
+{
+  public float health = 50f;
+
+  public void TakeDamege(float amount)
+  {
+    health -= amount;
+    if (health <= 0)
+    {
+      Die();
+    }
+  }
+
+  void Die()
+  {
+    Destroy(gameObject);
+  }
+}
